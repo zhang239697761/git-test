4def0107bf8fc48454d56f6e885707f2700a2fc8
zhang <zhang>
2019-07-06 08:55:51 +0800

    把工程添加到本地仓库

---
+++ git-test-project/src/pri/zhang/git/FirstGit.java
@@ -0,0 +1,12 @@
+package pri.zhang.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+        System.out.println("hello,world!");
+        System.out.println("hello,world!");
+        System.out.println("hello,world!");
+        System.out.println("hello,world!");
+        System.out.println("hello,world!");
+    }
+}
